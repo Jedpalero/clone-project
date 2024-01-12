5f048c49adca6e266b9a60eeccd3ea74f649e0a6
Jed Palero <raven@developer.dev>
2024-01-12 10:44:31 +0800

    Fixing short section responsiveness

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,8 +1,10 @@
 // import TagSection from "./TagSection";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Card from "./Card";
 import ShortsCard from "./ShortsCard";
 import { useState } from "react";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const LandingSection = ({ sidebar }) => {
   const [show, setShow] = useState(false);
@@ -10,15 +12,13 @@ const LandingSection = ({ sidebar }) => {
   return (
     // w-screen top-1/2 left-1/2
     <div
-      className={`flex flex-col w-full md:w-auto justify-center md:justify-start absolute mt-3 top-[110px]  text-white ${
+      className={`flex flex-col w-full md:w-auto md:items-start justify-center md:justify-start 4xl:ml-0 absolute mt-3 ml-3 top-[110px] text-white ${
         sidebar ? "md:left-[260px] " : "md:left-[110px]"
       }`}
     >
       <Card sidebar={sidebar} />
       <div
-        className={`flex items-center gap-3${
-          sidebar ? "" : "inline-block items-center"
-        }`}
+        className={`flex  gap-3${sidebar ? "" : "inline-block items-center"}`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -40,10 +40,26 @@ const LandingSection = ({ sidebar }) => {
         </svg>
         <h3 className="text-xl font-semibold">Shorts</h3>
       </div>
-      <div className="scrollbar absolute top-[100%] overflow-x-hidden">
-        <ShortsCard sidebar={sidebar} show={show} />
-        <div onClick={() => setShow(!show)} className="cursor-pointer">
-          show more
+      <div className="md:overflow-x-hidden ">
+        <div className="absolute top-[100%] ">
+          <ShortsCard sidebar={sidebar} show={show} />
+          <div
+            onClick={() => setShow(!show)}
+            className="relative border w-[300px] p-2 cursor-pointer text-center rounded-full 4xl:ml-[700px] md:ml-[500px] flex items-center justify-center gap-2 m-4"
+          >
+            {show ? (
+              <>
+                <h3>show less</h3>
+                <FontAwesomeIcon icon={faChevronUp} />
+              </>
+            ) : (
+              <>
+                <h3>show more</h3>
+                <FontAwesomeIcon icon={faChevronDown} />
+              </>
+            )}
+          </div>
+          {/* <hr className="w-[99%] absolute bottom-[20px] overflow-hidden z-0" /> */}
         </div>
       </div>
     </div>
--- src/components/ShortsCard.js
+++ src/components/ShortsCard.js
@@ -1,23 +1,37 @@
 import cards from "../data/shortCardDetails";
+// import { useState } from "react";
 
 const ShortsCard = ({ sidebar, show }) => {
+  // const [display, setDisplay] = useState(800);
+
+  // {cards.slice(0, ImageCount()).map((card) => (
+  function ImageCount() {
+    // to target the specific screen size(width);
+    let w = parseInt(window.innerWidth);
+    if (w < 1000) {
+      return 2;
+    }
+  }
+
   return (
     <div
-      className={`md:space-x-3 gap-3 mt-3 ${show ? "flex flex-wrap" : "flex"}`}
+      className={`md:space-x-1 md:gap-3 gap-4 mt-3  ${
+        show ? "flex flex-wrap  " : "flex overflow-hidden"
+      }`}
     >
-      {cards.map((card) => (
+      {cards.slice(0, ImageCount()).map((card) => (
         <div
           key={card.title}
           className={` cursor-pointer ${
             sidebar
-              ? "4xl:h-[550px] 4xl:w-[250px] md:h-[380px] md:w-[380px] h-[320px] w-[340px]"
-              : "4xl:h-[550px] 4xl:w-[250px] md:h-[310px] md:w-[330px] h-[320px] w-[340px]"
+              ? "4xl:h-[550px] 4xl:w-[260px] md:h-[520px] md:w-[233px] h-[440px] w-[170px]"
+              : "4xl:h-[550px] 4xl:w-[240px] md:h-[520px] md:w-[216px] h-[440px] w-[164px]"
           }`}
         >
           <img
             alt="thumbnail"
             src="https://i.ytimg.com/vi/WqKd3mHYeA8/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLDq35aEEdAIrK1SGGneOyDCTAe-0Q"
-            className="rounded-xl"
+            className="rounded-2xl"
           />
           <div className=" gap-3 mt-2">
             <div>
