207c4ac89205df7430d8c1516d77916fcd327cb8
Jed Palero <raven@developer.dev>
2024-01-16 15:05:13 +0800

    Adding details to home page and intial setup for search page

--- src/data/cardDetails.js
+++ src/data/cardDetails.js
@@ -1,93 +1,62 @@
 const cards = [
   {
     imgLink:
-      "https://i.ytimg.com/vi/Mos5QJAje28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLCFbVeOdpHjPmjEkLBCOpvPJC5eMg",
-    logoLink:
-      "https://yt3.ggpht.com/ytc/AIf8zZRa_GyvmUD5k59-H2EPR41y0YF_LhRy9gJTgOGR=s68-c-k-c0x00ffffff-no-rj",
-    title: "Fully Functional Youtube Clone | React | Redux | Firebase...",
-    channelName: "Backbench Coder",
-    details: "100k views . 2 years ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb3fa351bcd2f7a8f06a7bc3ba/1/en/default",
+
+    title: "Daily Mix 1",
+    channelName:
+      "Hillsong Worship, Hillsong UNITED, Every Nation Music and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/84dYijIpWjQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBK8CU84pqvJt9PGo_UKxG9M5y3uQ",
-    logoLink:
-      "https://yt3.ggpht.com/RpRPtZhc7dokY3Bkw-yg9kIr9G7_SxPuL_F-YouM6BT4B4h7fPxAY8sS3juhbO5ckTM3g8tIKg=s68-c-k-c0x00ffffff-no-rj",
-    title: "Fully How to Build Willpower | David Goggins & Dr. Andrew Huberman",
-    channelName: "Huberman Lab Clips",
-    details: "1M views . 6 days ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebba31840f1e1a7093f8e2d42b/2/en/default",
+
+    title: "Daily Mix 2",
+    channelName: "Mass Anthem, Cochren & Co., Stellar Kart and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/X6PwyH0thU0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBOit1ON2kx2XdjznBlzx1KX3XRPw",
-    logoLink:
-      "https://yt3.ggpht.com/ytc/AIf8zZQ7-hDaaiDs08cSeJl9pMXd5KUvrB7ZYii0XVaizw=s68-c-k-c0x00ffffff-no-rj",
-    title: "Portfolio So Good I Hired Him | Web Developer Portfolio Review",
-    channelName: "CodingPhase",
-    details: "72k views . 2 years ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb9a94208ff49d40929c302e1e/3/en/default",
+
+    title: "Daily Mix 3",
+    channelName: "CityAlight, Don Marsh, Dave Hunt and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/ru3U8MHbFFI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDCrQJiu3wRFRU91De8WQNxfm4c-A",
-    logoLink:
-      "https://yt3.ggpht.com/ytc/AIf8zZRxB4nFdEqcC_k03diwmuSrFDlqfFEI_sv7fkc19g=s68-c-k-c0x00ffffff-no-rj",
-    title: "Use these instead of vh",
-    channelName: "Kevin Powell",
-    details: "279k views . 1 month ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb097877542866c981757e9592/4/en/default",
+
+    title: "Daily Mix 4",
+    channelName: "TAEYEON, Billlie, Sunwoojunga and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/b-EvF9hNyB8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAAi2SGqjUhdFmGiQxHEbLvSoG9AQ",
-    logoLink:
-      "https://yt3.ggpht.com/fckeWKN9TKr7xJtolGdgf9LfR0pC6ICsg9YgWPaJym8J0rxOEKBp2xV748q9p3OhRObaKJUxOg=s68-c-k-c0x00ffffff-no-rj",
-    title: "Am I Too Old To Learn To Code + Change Career?",
-    channelName: "Code Questing",
-    details: "499 views . 16 hours ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb0610877c41cb9cc12ad39cc0/5/en/default",
+
+    title: "Daily Mix 5",
+    channelName: "Stray Kids, BTS, ENHYPEN and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/LMSVPAObR3E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDUYJ1ev7OOkzP5xV3UEXC6km50oQ",
-    logoLink:
-      "https://yt3.ggpht.com/pGRIfogWWCKMVH_HsALQFg3g7MjJxMzzGksJvCO1y6vVSIoNFRnpY0xOMfJH28bLXxraxqkt4A=s68-c-k-c0x00ffffff-no-rj",
-    title: "Leetcode Is Impossible. Can I Still get a Software Job?",
-    channelName: "Created By KC",
-    details: "7.7k views . 13 hours ago",
+      "https://dailymix-images.scdn.co/v2/img/ab6761610000e5eb11664298bf538ddd205f41df/6/en/default",
+
+    title: "Daily Mix 6",
+    channelName: "Hikaru Utada, YOASOBI, takayan and more",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/NT299zIk2JY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqZPjIgBJ_NTf7TnAGCXmQQ5C85Q",
-    logoLink:
-      "https://yt3.ggpht.com/ytc/AIf8zZRDiiescilO4rYZXxwz6KSGMNFSiw0HxvQ9C8cvpw=s68-c-k-c0x00ffffff-no-rj",
-    title: "🔴 Let's Build a YouTube Clone with REACT JS for Beginners",
-    channelName: "Clever Programmer",
-    details: "309k views . Streamed 3 years ago",
+      "https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/fnElnM7OJ0IOqKL5rmdyjaNXPDcEQZHLA4GWhGSmLDv7MO_gEsTA_3KaOD5lonbS6pZr1xFkgtydO03_Q_bBgzc7yJ-RJa-w3hR_qMA82o2JACoYzGAhVjTbbCnP0GX56JGvBu49u2bSCCmgVgExGiwhlEvCCkWLDz-IFi7-QuSvudjYgSgkfaog_D0iB1H4AtrY9LAOlHlnVGvrmcwgbUdUp_-QcjCddCae20KxsgsjqSQ3Ql_Cly-6RyiTvqvK/ODU6MzQ6NzFUNDItMTAtNA==",
+
+    title: "Discover Weekly",
+    channelName:
+      "Your weekly mixtape of fresh music. Enjoy new music and deep cuts picked for you. Updates every Monday.",
   },
   {
     imgLink:
-      "https://i.ytimg.com/vi/U4eDmWvcmA8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB05JB55TbsGaIIoh2x4DyKHvh6cg",
-    logoLink:
-      "https://yt3.ggpht.com/CVhwL18vqxu3M71z0FmXiypEdPyXcxWyMn7rVdocljLMDj05kimDsipwVuFmIJQ2OxZqqQDL=s68-c-k-c0x00ffffff-no-rj",
-    title: "How Boredom Leads To Greatness",
-    channelName: "Mindful",
-    details: "446k views . 2 months ago",
-  },
-  {
-    imgLink:
-      "https://i.ytimg.com/vi/-TwzqzXSnck/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhBKR0fX6s9a8q_4OTSy-TiNOaUQ",
-    logoLink:
-      "https://yt3.ggpht.com/E5hh4r9_0TRfAKSKVvXuSvQRLxCfv4Ow_SO-Llm_uwh08U_-SPbknLucATWPBtUiccFUUy-dXA=s68-c-k-c0x00ffffff-no-rj",
-    title: "I Asked Microsoft Software Engineers How To Get Hired",
-    channelName: "Namanh Kapur",
-    details: "248k views . 1 months ago",
-  },
-  {
-    imgLink:
-      "https://i.ytimg.com/vi/tS7upsfuxmo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCWMV-BF5X8M1vQoXP6PGghFjWuBQ",
-    logoLink:
-      "https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s68-c-k-c0x00ffffff-no-rj",
-    title: "Tailwind CSS Full Course 2023 | Build and Deploy a Nike Website",
-    channelName: "JavaScript Mastery",
-    details: "352k views . 4 months ago",
+      "https://newjams-images.scdn.co/image/ab67647800003f8a/dt/v3/release-radar/ab6761610000e5ebba31840f1e1a7093f8e2d42b/en",
+
+    title: "Release Radar",
+    channelName:
+      "Catch all the latest music from artists you follow, plus new singles picked for you. Updates every Friday.",
   },
 ];
 
